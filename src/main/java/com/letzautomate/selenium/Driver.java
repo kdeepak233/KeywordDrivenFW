24600304f8ce3c4d4af6e8ea3cf88c1a7b723fa5
Ramakrishna <letzautomate@gmail.com>
2018-07-18 12:09:08 +0530

    Driver file is created and initial code is written to invoke the browser

--- src/main/java/com/letzautomate/selenium/Driver.java
+++ src/main/java/com/letzautomate/selenium/Driver.java
@@ -1,10 +1,16 @@
 package com.letzautomate.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Driver {
 	
 	public static void main(String[] args){
 		
-		
+		String projPath = System.getProperty("user.dir");
+		System.out.println(projPath);
+		System.setProperty("webdriver.chrome.driver", projPath+"/src/test/resources/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 	}
 
 }
